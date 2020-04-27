c8fb56802c139de81b2338c7ef47ff8373d15d86
thisisabhinay <a8h1nay@gmail.com>
2020-04-28 02:45:27 +0530

    Added images section

--- src/App.js
+++ src/App.js
@@ -2,34 +2,19 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import Slider from './Views/Slider';
+import Quote from './Views/Quote';
 // Import css files
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
-import Full1 from './Assets/full_image1.jpg';
-import Full2 from './Assets/full_image2.jpg';
-import Full3 from './Assets/full_image3.jpg';
-import Full4 from './Assets/full_image4.jpg';
+import FullImage from './Views/FullImage';
 
 function App() {
-  const imgResponsive = {
-    display: 'block',
-    maxWidth: '100%',
-    height: 'auto',
-  };
-  const img = {
-    width: '100vw',
-  };
-
   return (
     <div>
       <Slider />
-      <div>
-        <img src={Full1} alt={'India'} style={Object.assign(imgResponsive, img)} />
-        <img src={Full2} alt={'Singapore'} style={Object.assign(imgResponsive, img)} />
-        <img src={Full3} alt={'USA'} style={Object.assign(imgResponsive, img)} />
-        <img src={Full4} alt={'Canada'} style={Object.assign(imgResponsive, img)} />
-      </div>
-      <div className="App">
+      <Quote />
+      <FullImage />
+      {/* <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <p>
@@ -44,7 +29,7 @@ function App() {
             Learn React
           </a>
         </header>
-      </div>
+      </div> */}
     </div>
   );
 }
