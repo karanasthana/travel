c9bc4a53fb08e67efe49ed4f559b74173b2078d1
thisisabhinay <a8h1nay@gmail.com>
2020-05-14 11:03:57 +0530

    Further completed the state page

--- src/Views/StatePicture.js
+++ src/Views/StatePicture.js
@@ -3,7 +3,7 @@ import React from 'react';
 export default function StatePicture(props) {
     return (
         <div style={{backgroundImage: `url('${props.image}')`, height: '30rem', width: '100%', backgroundSize: 'cover', display: 'flex', textAlign: 'center', backgroundRepeat: 'no-repeat', backgroundPosition: 'center' }} >
-            <h2 style={{fontSize: '10rem', marginTop: '15rem', width: '100%'}}>{props.stateName}</h2>
+            <h2 style={{ margin: '15rem 0', width: '100%', backgroundColor: '#dddddd44'}}>{props.stateName}</h2>
         </div>
     );
 }
--- src/Views/StatePlace.js
+++ src/Views/StatePlace.js
@@ -4,10 +4,10 @@ import { Card } from 'react-bootstrap';
 export default function StatePicture(props) {
     return (
         <Card style={{ display: 'inline-block', textAlign: 'center', padding: '30px', border: 'none' }} className="col-md-4 col-sm-6 col-xs-12" >
-            <div style={{ border: '1px solid rgba(0,0,0,.125)', borderRadius: '5px' }}>
-                <Card.Img variant="top" src={`${props.picture}`} style={{ height: '10rem', width: 'auto' }} />
-                <Card.Body style={{ textAlign: 'center' }}>
-                    <Card.Title>{`${props.place}`}</Card.Title>
+            <div style={{ border: '1px solid rgba(0,0,0,.125)', borderRadius: '5px', padding: '1px', backgroundColor: '#178be7' }}>
+                <Card.Img variant="top" src={`${props.picture}`} style={{ height: '10rem', width: 'auto', maxWidth: '10rem' }} />
+                <Card.Body style={{ textAlign: 'center', padding: '0.7rem', marginTop: '10px', borderTop: '1px solid #BBB', backgroundColor: 'white' }}>
+                    <Card.Title style={{ fontSize: '1.5rem' }}>{`${props.place}`}</Card.Title>
                 </Card.Body>
             </div>
         </Card>
