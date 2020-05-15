693d9cd94de6faa28b775b6cf804eaada6f5083c
thisisabhinay <a8h1nay@gmail.com>
2020-05-16 03:06:47 +0530

    s/class/className

--- src/Views/CountriesSection.js
+++ src/Views/CountriesSection.js
@@ -9,23 +9,23 @@ export default class CountriesSection extends React.Component {
 
     render() {
       const items = this.props.json.places.map((item, key) =>
-      <Link to="/states" class="card-sm" style={{backgroundImage: `url(${item.picture})`}}>
+      <Link to="/states" className="card-sm" style={{backgroundImage: `url(${item.picture})`}}>
         {
           item.place ? 
-          <p class="card__text-sm">{`${item.place}`}</p> : 
+          <p className="card__text-sm">{`${item.place}`}</p> : 
           null
         }
-        <h1 class="card__text-lg">{`${item.name}`}</h1>
+        <h1 className="card__text-lg">{`${item.name}`}</h1>
       </Link>
       );
         return (
-            <div class="main main-div" style={{backgroundImage: `url(${this.props.json.picture})` }}>
-              <section class="card-lg__container">
-                <p class="card__text-sm">{`${this.props.json.countryName}`}</p>
-                <h1 class="card__text-lg">{`${this.props.json.countryText}`}</h1>
-              {/* <p class="card__text-md">something something</p> */}
+            <div className="main main-div" style={{backgroundImage: `url(${this.props.json.picture})` }}>
+              <section className="card-lg__container">
+                <p className="card__text-sm">{`${this.props.json.countryName}`}</p>
+                <h1 className="card__text-lg">{`${this.props.json.countryText}`}</h1>
+              {/* <p className="card__text-md">something something</p> */}
               </section>
-              <section class="card-sm__container">
+              <section className="card-sm__container">
                 {items}
               </section>
             </div>
