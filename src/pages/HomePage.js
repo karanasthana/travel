038199219b1249859743cc61aed0b37b9c10d7c4
Karan Asthana <karan.asthana1@gmail.com>
2020-06-29 00:29:29 +0530

    added section for USA

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -2,17 +2,19 @@ import React from 'react';
 import CountriesSection from "../Views/CountriesSection";
 import India from '../staticData/countryData/India.json';
 import Singapore from '../staticData/countryData/Singapore.json';
+import USA from '../staticData/countryData/USA.json';
 import Quote from '../Views/Quote';
 // import Slider from '../Views/Slider';
 
 export default function HomePage() {
     return (
         <div>
-            <Quote quote={'Climb the Mountain so YOU can see the WORLD, not so the WORLD can see YOU'}/>
             <CountriesSection json={India} />
             <Quote quote={'Live with no excuses and travel with no regrets'}/>
             <CountriesSection json={Singapore} />
             <Quote quote={'The World is a book and those who do not travel read only a page'}/>
+            <CountriesSection json={USA} />
+            <Quote quote={'Climb the Mountain so YOU can see the WORLD, not so the WORLD can see YOU'}/>
             {/* <Slider />     */}
         </div>
     )
