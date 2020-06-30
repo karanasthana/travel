06b11702788fcf1870f65f41c22bbe0e66154bac
Karan Asthana <karan.asthana1@gmail.com>
2020-07-01 03:20:37 +0530

    added links and json for up

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -3,6 +3,7 @@ import CountriesSection from "../Views/CountriesSection";
 import India from '../staticData/countryData/India.json';
 import Singapore from '../staticData/countryData/Singapore.json';
 import USA from '../staticData/countryData/USA.json';
+import England from '../staticData/countryData/England.json';
 import Quote from '../Views/Quote';
 // import Slider from '../Views/Slider';
 
@@ -15,7 +16,8 @@ export default function HomePage() {
             <CountriesSection json={Singapore} />
             <Quote quote={'Climb the Mountain so YOU can see the WORLD, not so the WORLD can see YOU'}/>
             <CountriesSection json={USA} />
-            <Quote quote={'To awaken quite alone in a strange town is one of the most pleasant sensations in the world'}/>
+            <Quote quote={'To wake up alone in a strange town is one of the most pleasant sensations in the world'}/>
+            <CountriesSection json={England} />
             {/* <Slider />     */}
         </div>
     )
