6a6c927a2288dd56f62f636b5d6b2e14eabe5cb8
Karan Asthana <karan.asthana1@gmail.com>
2020-09-01 21:32:22 +0530

    fixed border of the heading

--- src/Views/StatePicture.js
+++ src/Views/StatePicture.js
@@ -3,7 +3,7 @@ import React from 'react';
 export default function StatePicture(props) {
     return (
         <div style={{backgroundImage: `url('${props.image}')`, height: '30rem', width: '100%', backgroundSize: 'cover', display: 'flex', alignItems: 'center', justifyContent: 'center', backgroundRepeat: 'no-repeat', backgroundPosition: 'center' }} >
-            <h2 style={{ backgroundColor: 'white', padding: '5px' }}>{props.stateName}</h2>
+            <h2 style={{ backgroundColor: 'white', padding: '5px', border: '1px solid' }}>{props.stateName}</h2>
         </div>
     );
 }
