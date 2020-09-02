1db5dfab4e4c74bdef9ec0ec61d92f9b56f10cf9
Karan Asthana <karan.asthana1@gmail.com>
2020-09-03 00:58:31 +0530

    fixed some styling and added modal

--- src/Views/StatePlace.js
+++ src/Views/StatePlace.js
@@ -1,10 +1,21 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Card } from 'react-bootstrap';
 import './../css/StatePlace.css';
+import Modal from 'react-bootstrap/Modal'
 
 export default function StatePlace(props) {
+    const [smShow, setSmShow] = useState(false);
+    
+    let handleShow = () => {
+        console.log('open the dialog for' + JSON.stringify(props));
+        if (props.hasDetails || true) {
+            setSmShow(true);
+        }
+    };
+
     return (
-        <Card className="cardContainer" >
+        <div>
+        <Card className="cardContainer" onClick={handleShow}>
             <div className="cardOuterDiv">
                 <Card.Img variant="top" src={`${props.picture}`} className="cardImg" />
                 <Card.Body className="cardBody">
@@ -12,5 +23,20 @@ export default function StatePlace(props) {
                 </Card.Body>
             </div>
         </Card>
+        <Modal
+            size="lg"
+            show={smShow}
+            onHide={() => setSmShow(false)}
+            aria-labelledby="example-modal-sizes-title-sm"
+            dialogClassName="place-dialog"
+        >
+            <Modal.Header closeButton>
+            <Modal.Title id="example-modal-sizes-title-sm">
+                {props.place}
+            </Modal.Title>
+            </Modal.Header>
+            <Modal.Body>...</Modal.Body>
+        </Modal>
+        </div>
     );
 }
