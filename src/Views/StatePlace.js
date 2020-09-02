c001624b06ba5c7f03ea85f500e0c16ee598b349
Karan Asthana <karan.asthana1@gmail.com>
2020-09-03 01:20:24 +0530

    fixed styling

--- src/Views/StatePlace.js
+++ src/Views/StatePlace.js
@@ -4,39 +4,39 @@ import './../css/StatePlace.css';
 import Modal from 'react-bootstrap/Modal'
 
 export default function StatePlace(props) {
-    const [smShow, setSmShow] = useState(false);
+    const [showModal, setShowModal] = useState(false);
     
     let handleShow = () => {
         console.log('open the dialog for' + JSON.stringify(props));
-        if (props.hasDetails || true) {
-            setSmShow(true);
+        if (props.hasDetails) {
+            setShowModal(true);
         }
     };
 
     return (
         <div>
-        <Card className="cardContainer" onClick={handleShow}>
-            <div className="cardOuterDiv">
-                <Card.Img variant="top" src={`${props.picture}`} className="cardImg" />
-                <Card.Body className="cardBody">
-                    <Card.Title className="cardTitle">{`${props.place}`}</Card.Title>
-                </Card.Body>
-            </div>
-        </Card>
-        <Modal
-            size="lg"
-            show={smShow}
-            onHide={() => setSmShow(false)}
-            aria-labelledby="example-modal-sizes-title-sm"
-            dialogClassName="place-dialog"
-        >
-            <Modal.Header closeButton>
-            <Modal.Title id="example-modal-sizes-title-sm">
-                {props.place}
-            </Modal.Title>
-            </Modal.Header>
-            <Modal.Body>...</Modal.Body>
-        </Modal>
+            <Card className="cardContainer" onClick={handleShow}>
+                <div className="cardOuterDiv">
+                    <Card.Img variant="top" src={`${props.picture}`} className="cardImg" />
+                    <Card.Body className="cardBody">
+                        <Card.Title className="cardTitle">{`${props.place}`}</Card.Title>
+                    </Card.Body>
+                </div>
+            </Card>
+            <Modal
+                size="lg"
+                show={showModal}
+                onHide={() => setShowModal(false)}
+                aria-labelledby="example-modal-sizes-title-sm"
+                dialogClassName="place-dialog"
+            >
+                <Modal.Header closeButton>
+                    <Modal.Title id="example-modal-sizes-title-sm">
+                        {props.place}
+                    </Modal.Title>
+                </Modal.Header>
+                <Modal.Body>...</Modal.Body>
+            </Modal>
         </div>
     );
 }
