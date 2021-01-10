e900666302e4f554cbffc509d9100c8f63cc1bee
Karan Asthana <karan.asthana1@gmail.com>
2021-01-10 22:32:22 +0530

    added link icon and overflow-wrap for heading

--- src/Views/CountriesSection.js
+++ src/Views/CountriesSection.js
@@ -22,7 +22,12 @@ export default class CountriesSection extends React.Component {
       <Link to={{pathname: `/travel/states/:${item.id}`, id:`${item.id}`, name: `${item.name}` }} id={item.id} className="card-sm" style={{backgroundImage: `url(${item.picture})`}}>
         {
           item.place ? 
-          <p className="card__text-sm">{`${item.place}`}</p> : 
+          <div style={{display: 'flex'}}>
+            <p className="card__text-sm">
+              <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="10" height="10" style={{ backgroundColor: 'white' }}><path fill="none" d="M0 0h24v24H0z"/><path d="M17 17h5v2h-3v3h-2v-5zM7 7H2V5h3V2h2v5zm11.364 8.536L16.95 14.12l1.414-1.414a5 5 0 1 0-7.071-7.071L9.879 7.05 8.464 5.636 9.88 4.222a7 7 0 0 1 9.9 9.9l-1.415 1.414zm-2.828 2.828l-1.415 1.414a7 7 0 0 1-9.9-9.9l1.415-1.414L7.05 9.88l-1.414 1.414a5 5 0 1 0 7.071 7.071l1.414-1.414 1.415 1.414zm-.708-10.607l1.415 1.415-7.071 7.07-1.415-1.414 7.071-7.07z"/></svg>
+              {`${item.place}`}
+            </p>
+          </div> : 
           null
         }
         <h1 className="card__text-lg">{`${item.name}`}</h1>
