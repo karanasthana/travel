4270ff6d3cc93a443a69b450ac7171014c69f6c0
Karan Asthana <karan.asthana1@gmail.com>
2021-02-07 21:10:39 +0530

    removed commented out code

--- src/App.js
+++ src/App.js
@@ -1,38 +1,20 @@
 import React from 'react';
 import {
   HashRouter as Router,
-  // BrowserRouter as Router,
   Switch,
   Route,
 } from "react-router-dom";
-// import logo from './logo.svg';
 import './App.css';
-// Import css files
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
-// import FullImage from './Views/FullImage';
-// import India from './Views/India';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import HomePage from './pages/HomePage';
 import StatesPage from './pages/StatesPage';
-// import Himachal from './staticData/statesData/Himachal.json';
 
 function App() {
   return (
     <Router>
       <div>
-        {/* <nav>
-          <ul>
-            <li>
-              <Link to="/">Home</Link>
-            </li>
-            <li>
-              <Link to="/states">Users</Link>
-            </li>
-          </ul>
-        </nav>
-        <div>
-        </div> */}
           {/* A <Switch> looks through its children <Route>s and
               renders the first one that matches the current URL. */}
         <Switch>
